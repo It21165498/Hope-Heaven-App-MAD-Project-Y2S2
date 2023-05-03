561f68fd9dc127708efc4a02c61c6e30dc4df3a5
IT21181856 <it21181856@my.sliit.lk>
2023-05-03 20:09:33 +0530

    wip

--- app/src/main/java/com/example/hopeheaven/DonorModel.kt
+++ app/src/main/java/com/example/hopeheaven/DonorModel.kt
@@ -2,7 +2,7 @@ package com.example.hopeheaven
 
 import android.text.Editable
 
-data class Donor(
+data class DonorModel(
     var fullName:String?=null, var age: Editable ,
     var from:String?=null, var phone: Editable , var email: String?=null,
     var password:String?=null, var donorId:String?=null){
--- app/src/main/java/com/example/hopeheaven/DonorRegister.kt
+++ app/src/main/java/com/example/hopeheaven/DonorRegister.kt
@@ -229,8 +229,8 @@ class DonorRegister : AppCompatActivity() {
 
         val donorId=dbRef.push().key!!
 
-        val donor = Donor(name,age ,from,phone,email,password,donorId)
-        dbRef.child(donorId).setValue(donor)
+        val donorModel = DonorModel(name,age ,from,phone,email,password,donorId)
+        dbRef.child(donorId).setValue(donorModel)
             .addOnCompleteListener(){
                 Toast.makeText(this,"data inserted successfully",Toast.LENGTH_LONG).show()
             }.addOnFailureListener(){
