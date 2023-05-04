44d123ccbfca8505400a93e71a119982b55e1a15
It21165498 <It21165498@my.sliit.lk>
2023-05-04 15:15:33 +0530

    student and donor profile conditionally render

--- app/src/main/java/com/example/hopeheaven/MainActivity.kt
+++ app/src/main/java/com/example/hopeheaven/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.example.hopeheaven.databinding.ActivityMainBinding
 import com.example.hopeheaven.databinding.ActivityStudentRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,12 +17,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         replaceFragment(Home())
 
+
         binding.NavBar.setOnItemSelectedListener {
             when(it.itemId){
                 R.id.icon_home -> replaceFragment(Home())
                 R.id.icon_students -> replaceFragment(StudentsList())
-//                R.id.icon_donors -> replaceFragment()
-                R.id.icon_profile -> replaceFragment(StudentProfile())
+//                R.id.icon_donors -> replaceFragment(all donor fetching fragment should be here)
+                R.id.icon_profile -> if(isStudent()){
+                    replaceFragment(StudentProfile())
+                }else{
+//                    replaceFragment(donorprofile fragment shoulb be here)
+                }
 
                 else -> {
 
@@ -40,4 +47,35 @@ class MainActivity : AppCompatActivity() {
         fragmentTransaction.commit()
 
     }
+
+
+    private fun isStudent(): Boolean {
+        var isStudent = false // default value
+        val user = FirebaseAuth.getInstance().currentUser // Get the Firebase Authentication user object
+        val fireStoreDatabase = FirebaseFirestore.getInstance() //Get firestore db
+
+        fireStoreDatabase.collection("Users")
+            .document(user?.uid.toString())
+            .get()
+            .addOnSuccessListener { documentSnapshot ->
+                if (documentSnapshot.exists()) {
+                    val userType = documentSnapshot.getString("userType")
+
+                    if (userType == "Student"){
+                        isStudent = true
+                    } else {
+                        isStudent = false
+                    }
+
+                } else {
+                    // document does not exist
+                }
+            }
+            .addOnFailureListener { exception ->
+                // handle errors here
+            }
+        return isStudent
+    }
+
+
 }
