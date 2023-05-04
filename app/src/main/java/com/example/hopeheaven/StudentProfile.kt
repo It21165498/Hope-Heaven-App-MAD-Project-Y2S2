3c78570029040f6c27ef516299fa364a988bac86
It21165498 <It21165498@my.sliit.lk>
2023-05-05 02:03:29 +0530

    logout

--- app/src/main/java/com/example/hopeheaven/StudentProfile.kt
+++ app/src/main/java/com/example/hopeheaven/StudentProfile.kt
@@ -1,59 +1,81 @@
 package com.example.hopeheaven
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import com.example.hopeheaven.databinding.FragmentStudentProfileBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [StudentProfile.newInstance] factory method to
- * create an instance of this fragment.
- */
 class StudentProfile : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentStudentProfileBinding
+    // Get the Firebase Authentication user object
+    lateinit var auth : FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_student_profile, container, false)
-    }
+        binding = FragmentStudentProfileBinding.inflate(inflater, container, false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment StudentProfile.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            StudentProfile().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+        auth = Firebase.auth
+        var user = auth.currentUser
+        val fireStoreDatabase = FirebaseFirestore.getInstance()
+
+        fireStoreDatabase.collection("Users").document(user?.uid.toString()).get()
+            .addOnSuccessListener { documentSnapshot ->
+                if (documentSnapshot.exists()) {
+                    // Get the data from the document snapshot
+                    val age = documentSnapshot.getString("age").toString()
+                    val email = documentSnapshot.getString("email").toString()
+                    val from = documentSnapshot.getString("from").toString()
+                    val gender = documentSnapshot.getString("gender").toString()
+                    val name = documentSnapshot.getString("name").toString()
+                    val phone = documentSnapshot.getString("phone").toString()
+                    val school = documentSnapshot.getString("Student").toString()
+
+                    binding.tvNameInput.text = name
+                    binding.tvAgeInput.text = age
+                    binding.tvSclInput.text = school
+                    binding.tvPhoneInput.text = phone
+                    binding.tvEmailInput.text = email
+                    binding.tvFromInput.text = from
+                    binding.tvGenderInput.text = gender
+
+
+
+                } else {
+                    Log.d("", "No such document")
                 }
             }
+            .addOnFailureListener { exception ->
+                Log.d("", "Error getting document: $exception")
+            }
+
+        binding.tvLogOut.setOnClickListener {
+            auth.signOut()
+            // Get the hosting Activity
+            val activity = requireActivity()
+            val intent = Intent(activity, StudentLogin::class.java)
+            activity.startActivity(intent)// Start the StudentLogin activity
+            activity.finish() //finish current activity
+
+        }
+
+
+
+
+
+        return binding.root
     }
+
 }
